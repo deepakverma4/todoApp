fb01f3c60c8c6b6825c0061d7f2e088f981e1b97
deepak <er.deepakverma4u@gmail.com>
2018-03-25 13:14:05 +0530

    completed without offline data persistence

--- App.js
+++ App.js
@@ -1,58 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: '',
+			todo: []
+		};
+	}
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+	deleteTodo(t) {
+		var arr = this.state.todo;
+		var position = arr.indexOf(t);
+		arr.splice(position, 1);
+		this.setState({ todo: arr });
+	}
+
+	addTodo = () => {
+		var newTodo = this.state.text;
+		var arr = this.state.todo;
+		arr.push(newTodo);
+		this.setState({ todo: arr });
+	};
+
+	renderTodo = () => {
+		return this.state.todo.map(t => {
+			return (
+				<Text
+					key={t}
+					style={styles.todoStyle}
+					onPress={() => {
+						this.deleteTodo(t);
+					}}
+				>
+					{t}
+				</Text>
+			);
+		});
+	};
+	render() {
+		return (
+			<View style={styles.wrapper}>
+				<View style={styles.container}>
+					<Text style={styles.headerStyle}>ToDo App</Text>
+
+					<TextInput
+						style={styles.inputStyle}
+						underlineColorAndroid="transparent"
+						value={this.state.text}
+						onChangeText={text => this.setState({ text })}
+					/>
+
+					<Button title="Add" onPress={this.addTodo} />
+					<View style={{ marginTop: 50 }}>{this.renderTodo()}</View>
+				</View>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = {
+	wrapper: {
+		flex: 1,
+		backgroundColor: '#0288D1'
+	},
+	container: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 20
+	},
+	headerStyle: {
+		fontWeight: 'bold',
+		fontSize: 30,
+		color: 'white'
+	},
+	inputStyle: {
+		height: 40,
+		width: 300,
+		color: 'white',
+		borderColor: 'white',
+		borderWidth: 1
+	},
+	todoStyle: {
+		fontSize: 24,
+		color: 'white'
+	}
+};
